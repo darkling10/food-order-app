e0fa36d55cc7e5e960365ab9b5e6d380d82e08ae
darkling10 <pathan.abbas10@gmail.com>
2022-01-14 16:45:51 +0530

    Connected to a Firebase backend

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,36 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+
+import { useHttp } from "../../hooks/use-http";
 
 const AvailableMeals = () => {
-  const mealsitem = DUMMY_MEALS.map((meal) => (
+  const [meals, setmeals] = useState([]);
+  const { isLoading, error, sendRequest: fetchMeals } = useHttp();
+
+  const transformMeals = (responseData) => {
+    const loadedMeals = [];
+
+    for (const key in responseData) {
+      loadedMeals.push({
+        id: key,
+        name: responseData[key].name,
+        description: responseData[key].description,
+        price: responseData[key].price,
+      });
+    }
+
+    setmeals(loadedMeals);
+  };
+
+  useEffect(() => {
+    fetchMeals(
+      {
+        url: "https://food-cart-app-4fccf-default-rtdb.firebaseio.com/meals.json",
+      },
+      transformMeals
+    );
+    // fetchMeals();
+  }, [fetchMeals]);
+
+  // console.log(meals);
+  const mealsitem = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -39,10 +44,10 @@ const AvailableMeals = () => {
       price={meal.price}
     ></MealItem>
   ));
-
   return (
     <section className={classes.meals}>
       <Card>
+        {error && <p>{error}</p>}
         <ul>{mealsitem}</ul>
       </Card>
     </section>
---
+++ src/hooks/use-http.js
@@ -0,0 +1,34 @@
+import React, { useState, useCallback } from "react";
+
+export const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applydata) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applydata(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
