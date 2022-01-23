6dea73608285830be40e5dcf1b5b45d9f07eca1b
darkling10 <pathan.abbas10@gmail.com>
2022-01-23 16:01:36 +0530

    About Page added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Product />} />
-      </Routes>
-   
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Product />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
