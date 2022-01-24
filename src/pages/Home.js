1208793c0ac9b3e3a7f8906427bb1f6cc19701c1
darkling10 <pathan.abbas10@gmail.com>
2022-01-24 16:08:56 +0530

    Slideshow removed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "../components/Layout/Header";
-import Slideshow from "../components/Slideshow/Slideshow";
 
 const Home = () => {
   return (
     <Fragment>
       <Header showButton={false} showBanner={false}/>
       <main>
-        <Slideshow></Slideshow>
+        
       </main>
     </Fragment>
   );
