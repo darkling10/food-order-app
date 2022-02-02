f6e5a3f12ad9fe5db9234f5e9a79114f230fef39
darkling10 <pathan.abbas10@gmail.com>
2022-02-02 13:33:16 +0530

    Changes made

--- src/components/HomeBody/ExploreTabs/ExploreItem.jsx
+++ src/components/HomeBody/ExploreTabs/ExploreItem.jsx
@@ -4,7 +4,7 @@ import classes from "./ExploreItem.module.css";
 const ExploreItem = (props) => {
   return (
     <div className={classes.itemContainer}>
-      <div>
+      <div className={classes.imgHolder}>
         <img src={props.imgSrc} alt="order" />
       </div>
       <div>
